c792bdb58911247ab8536f94186ae079e10e4968
Daniel Llinas <danllinas@gmail.com>
2015-07-28 07:49:46 -0400

    STarted working on exchange logic.

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -2,14 +2,19 @@ class ExchangesController < ApplicationController
   def new
   end
 
+  def show
+    @exchange = Exchange.find(params[:id])
+  end
+
   def edit
   end
 
   def create
     @exchange = Exchange.new(exchange_params)
     if @exchange.save
+      @exchange.book.status = "requested"
       flash[:success] = "You requested the book!"
-      redirect_to :back
+      redirect_to exchange_path
     else
       flash[:alert] = "Something went wrong. Try agian."
       render books_path
